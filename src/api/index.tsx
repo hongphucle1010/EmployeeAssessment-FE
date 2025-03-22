18282924f54fb32e77abad6dc21d5527d48b81fb
Hong Phuc Le <72754942+lhplhp10102004@users.noreply.github.com>
2025-03-22 11:52:43 +0700

    feat: add backend API integration with axios and environment configuration

---
+++ src/api/index.tsx
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import { BACKEND_URL } from '../lib/helper/constant'
+import { getAccessToken } from '../lib/helper/authentication'
+
+const host = BACKEND_URL
+
+export const apiHost = `${host}`
+
+export const apiClient = axios.create({
+  baseURL: apiHost,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const accessToken = getAccessToken() // use helper function to get access token
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}` // set in header
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
