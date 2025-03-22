a043823f171f1fbc86b04757722bd37b8d9a5921
NhatHa <ha.ngnhnht@gmail.com>
2025-03-22 11:43:34 +0700

    login page

---
+++ src/pages/EmployeesPage/index.tsx
@@ -0,0 +1,9 @@
+const EmployeesPage = () => {
+    return (
+        <div className="flex flex-col items-start gap-4 text-black">
+            <span className="font-semibold text-xl">Employee List</span>
+        </div>
+    )
+}
+
+export default EmployeesPage
---
+++ src/pages/LoginPage/index.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "../../components/login"
+
+const LoginPage = () => {
+    return (
+        <div className="flex w-full h-full items-center justify-center">
+            <LoginForm />
+        </div>
+    )
+}   
+
+export default LoginPage
