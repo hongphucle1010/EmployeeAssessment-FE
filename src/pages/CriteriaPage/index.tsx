82a22b3d652acb1126bacbfde47394bfde7ddde0
Hong Phuc Le <72754942+lhplhp10102004@users.noreply.github.com>
2025-03-26 21:32:47 +0700

    feat: refactor CriteriaPage to use CriteriaService for data fetching and submission; update form handling and types

--- src/pages/CriteriaPage/index.tsx
+++ src/pages/CriteriaPage/index.tsx
@@ -1,33 +1,10 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
 
-import * as yup from 'yup';
-import { useForm, SubmitHandler } from "react-hook-form"
-import { yupResolver } from "@hookform/resolvers/yup"
-import { getAllCriteria, createCriteria } from "../../api/criteria";
-
-interface ICriteriaList {
-  name: string
-  description: string
-}
-
-// hard-coded data
-const CRITERIA_LIST = [
-  {
-    id: 1,
-    name: 'Hello Hello',
-    description: 'Happy Happy Happy HappyHappy HappyHappy HappyHappy HappyHappy Happy'
-  },
-  {
-    id: 2,
-    name: 'Hello Hello',
-    description: 'Happy Happy Happy HappyHappy HappyHappy HappyHappy HappyHappy Happy'
-  },
-  {
-    id: 3,
-    name: 'Hello Hello',
-    description: 'Happy Happy Happy HappyHappy HappyHappy HappyHappy HappyHappy Happy'
-  }
-]
+import * as yup from 'yup'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { CriteriaService } from '../../services/criteria'
+import { Criteria } from '../../api/criteria/types'
 
 const CriteriaItem = ({ title, item }: { title: string; item: string }) => {
   return (
@@ -40,7 +17,7 @@ const CriteriaItem = ({ title, item }: { title: string; item: string }) => {
   )
 }
 
-const CriteriaList = (props: ICriteriaList) => {
+const CriteriaList = (props: Criteria) => {
   return (
     <div className='w-full p-5 flex flex-col gap-4 rounded-lg border border-blue-600'>
       <div className='w-full flex flex-row items-center justify-between'>
@@ -61,17 +38,16 @@ const CriteriaList = (props: ICriteriaList) => {
   )
 }
 
+// Define a new type for form data that excludes the 'id' field
+type CriteriaForm = Omit<Criteria, 'id'>
+
 const CriteriaSchema = yup.object().shape({
   name: yup.string().required('Criteria name is required'),
   description: yup.string().required('Description is required')
 })
 
 const NewCriteriaListForm = ({ open, setOpen }: { open: boolean; setOpen: (open: boolean) => void }) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm<ICriteriaList>({
+  const { register, handleSubmit } = useForm<CriteriaForm>({
     resolver: yupResolver(CriteriaSchema),
     defaultValues: {
       name: '',
@@ -79,94 +55,103 @@ const NewCriteriaListForm = ({ open, setOpen }: { open: boolean; setOpen: (open:
     }
   })
 
-    const onSubmit: SubmitHandler<ICriteriaList> = async (data) => {
-        try {
-            await createCriteria(data)
-            console.log('Criteria created', data)
-            setOpen(!open)
-        } catch(error) {
-            console.error(error)
-        }
+  const onSubmit: SubmitHandler<CriteriaForm> = async (data) => {
+    try {
+      const response = await CriteriaService.createCriteria(data)
+      console.log('Criteria created', response)
+      setOpen(!open)
+    } catch (error) {
+      console.error(error)
     }
+  }
 
-    return (
-        <form className="w-full p-5 flex flex-col gap-4 rounded-lg border border-gray-300" onSubmit={handleSubmit(onSubmit)}>
-            <div className='flex flex-col gap-4 items-center justify-center'>
-                <span className="text-xl font-semibold text-left w-full">New Criteria</span>
-                <div className="w-full">
-                    <label className='font-semibold text-sm' htmlFor="CriteriaName">Name: </label>
-                    <input
-                        id='CriteriaName'
-                        type="text"
-                        className="w-full p-2 mt-1 border border-gray-300 rounded-md"
-                        placeholder="Criteria #1"
-                        {...register('name')}
-                    />
-                </div>
-                <div className="w-full">
-                    <label className='font-semibold text-sm' htmlFor="CriteriaDesc">Description: </label>
-                    <input
-                        id='CriteriaDesc'
-                        type="text"
-                        className="w-full p-2 mt-1 border border-gray-300 rounded-md"
-                        placeholder="Something about this criteria..."
-                        {...register('description')}
-                    />
-                </div>
-                <div className="flex flex-row items-center gap-3">
-                    <button
-                        className="px-3 py-2 rounded-md bg-gray-300 text-white"
-                        onClick={() => setOpen(!open)}
-                    >
-                        Cancel
-                    </button>
-                    <button
-                        type="submit"
-                        className="px-3 py-2 rounded-md bg-blue-600 text-white"
-                    >
-                        Save
-                    </button>
-                </div>
-            </div>
-        </form>
-    )
+  return (
+    <form
+      className='w-full p-5 flex flex-col gap-4 rounded-lg border border-gray-300'
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className='flex flex-col gap-4 items-center justify-center'>
+        <span className='text-xl font-semibold text-left w-full'>New Criteria</span>
+        <div className='w-full'>
+          <label className='font-semibold text-sm' htmlFor='CriteriaName'>
+            Name:{' '}
+          </label>
+          <input
+            id='CriteriaName'
+            type='text'
+            className='w-full p-2 mt-1 border border-gray-300 rounded-md'
+            placeholder='Criteria #1'
+            {...register('name')}
+          />
+        </div>
+        <div className='w-full'>
+          <label className='font-semibold text-sm' htmlFor='CriteriaDesc'>
+            Description:{' '}
+          </label>
+          <input
+            id='CriteriaDesc'
+            type='text'
+            className='w-full p-2 mt-1 border border-gray-300 rounded-md'
+            placeholder='Something about this criteria...'
+            {...register('description')}
+          />
+        </div>
+        <div className='flex flex-row items-center gap-3'>
+          <button
+            className='px-3 py-2 rounded-md bg-gray-300 text-white'
+            onClick={(e) => {
+              e.preventDefault()
+              setOpen(!open)
+            }}
+          >
+            Cancel
+          </button>
+          <button type='submit' className='px-3 py-2 rounded-md bg-blue-600 text-white'>
+            Save
+          </button>
+        </div>
+      </div>
+    </form>
+  )
 }
 
 const CriteriaPage = () => {
-    const [openNewForm, setOpenNewForm] = useState(false)
+  const [openNewForm, setOpenNewForm] = useState(false)
 
-    const [criteriaList, setCriteriaList] = useState<ICriteriaList[]>([]);
+  const [criteriaList, setCriteriaList] = useState<Criteria[]>([])
 
-    useEffect(() => {
-        const fetchCriteria = async () => {
-            try {
-                const response = await getAllCriteria();
-                setCriteriaList(response);
-                console.log("Criteria fetched", response);
-            } catch (error) {
-                console.error("Failed to fetch criteria", error);
-            }
-        };
+  useEffect(() => {
+    const fetchCriteria = async () => {
+      try {
+        const response = await CriteriaService.getCriterias()
+        setCriteriaList(response.data)
+        console.log('Criteria fetched', response)
+      } catch (error) {
+        console.error('Failed to fetch criteria', error)
+      }
+    }
 
-        fetchCriteria();
-    }, []);
+    fetchCriteria()
+  }, [])
 
-    return (
-        <div className="flex flex-col items-start gap-4 text-black p-5">
-            <span className="font-semibold text-2xl">My Criteria Collection</span>
-            {CRITERIA_LIST.map((item, index) => (
-                <CriteriaList key={index} name={item.name} description={item.description} />
-            ))}
-            {openNewForm && <NewCriteriaListForm open={openNewForm} setOpen={setOpenNewForm} />}
-            <button
-                disabled={openNewForm === true}
-                className="w-full p-5 rounded-lg border-dashed border border-blue-600 text-blue-600 disabled:border-gray-400 disabled:text-gray-400"
-                onClick={() => { setOpenNewForm(!openNewForm) }}
-            >
-                Add New Criteria
-            </button>
-        </div>
-    )
+  return (
+    <div className='flex flex-col items-start gap-4 text-black p-5'>
+      <span className='font-semibold text-2xl'>My Criteria Collection</span>
+      {criteriaList.map((item) => (
+        <CriteriaList key={item.id} id={item.id} name={item.name} description={item.description} />
+      ))}
+      {openNewForm && <NewCriteriaListForm open={openNewForm} setOpen={setOpenNewForm} />}
+      <button
+        disabled={openNewForm === true}
+        className='w-full p-5 rounded-lg border-dashed border border-blue-600 text-blue-600 disabled:border-gray-400 disabled:text-gray-400'
+        onClick={() => {
+          setOpenNewForm(!openNewForm)
+        }}
+      >
+        Add New Criteria
+      </button>
+    </div>
+  )
 }
 
 export default CriteriaPage
